cd79be99333370339f78bd0c683314993b41e0b2
Karissa McKelvey <krmckelv@gmail.com>
2014-10-30 17:57:35 -0700

    BUG: use the dirname for the test db.

--- example/config.js
+++ example/config.js
@@ -1,9 +1,11 @@
+var path = require('path')
+
 var config = {
   'DEBUG': true,
   'PORT': 5000,
   'HOSTNAME': 'http://localhost',
   'HOST': 'http://localhost:5000',
-  'DB': '../data',
+  'DB':  path.join(__dirname, '..', 'data', 'testdb')
 };
 
 module.exports = config;
